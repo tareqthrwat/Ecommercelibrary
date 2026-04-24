1c0b9a60bbd70207f38342a25b8caae05b96f1ef
tareqthrwat <tarekthrwat106@gmail.com>
2026-04-24 11:35:16 +0300

    fex-footer

--- src/layouts/Mainlayout.jsx
+++ src/layouts/Mainlayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../components/footer/Footer";
+import Footer from "../components/footer/footer";
 import Navbar from "../components/Navbar/Navbar";
 
 export default function MainLayout() {
